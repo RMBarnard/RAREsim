09ea48e5b2a2e288dc4bcdea7ea9304c55bd87f9
RMBarnard <rbarnard1107@gmail.com>
2024-03-15 00:45:27 -0600

    Re-organize for raresim package indepence from sim files

---
+++ convert.py
@@ -0,0 +1,40 @@
+import argparse
+from src.main.raresim.common.sparse import *
+import timeit
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i',
+                        dest='input_file',
+                        required=True,
+                        help='Input haplotype file path')
+
+    parser.add_argument('-o',
+                        dest='output_file',
+                        required=True,
+                        help='Output sparse matrix path')
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = get_args()
+    reader = SparseMatrixReader()
+    writer = SparseMatrixWriter()
+
+    start = timeit.default_timer()
+    matrix = reader.loadSparseMatrix(args.input_file)
+    end = timeit.default_timer()
+    print(f"Time taken: {end-start}")
+
+    start = timeit.default_timer()
+    writer.writeToHapsFile(matrix, args.output_file, "sm")
+    end = timeit.default_timer()
+    print(f"Time taken: {end-start}")
+
+if __name__ == '__main__':
+    main()
---
+++ src/main/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='raresim',
+    version='0.1',
+    description='Raresim library',
+    author='Ryan Barnard',
+    author_email='rbarnard1107@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+    ],
+)
